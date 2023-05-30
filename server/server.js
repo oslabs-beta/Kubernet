606c6bbc08acfef928f0c1fd1b03b428e857f719
Joseph-Caballero <123486138+Joseph-Caballero@users.noreply.github.com>
2023-05-30 17:41:59 -0400

    backend boilerplate finished

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+
+app.use(express.json());
+
+app.use((req, res) => res.status(404).send('Page Not Found'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});

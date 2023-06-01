f17776d057de61540c26406d4efad755adc24729
Paul <plee50@binghamton.edu>
2023-06-01 12:07:04 -0400

    Created mock dashboard

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,8 +1,46 @@
 import React, { Component, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from '../styles/Dashboard.module.scss';
 
 function Dashboard() {
-  return <div>W.I.P. Dashboard</div>;
+  return (
+    <>
+      <div className={styles.numericMetrics}>
+        <iframe
+          src='https://www.google.com/webhp?igu=1'
+          width='450'
+          height='200'
+          frameborder='0'
+        ></iframe>
+        <iframe
+          src='http://localhost:3000/d-solo/efa86fd1d0c121a26444b636a3f509a8/kubernetes-compute-resources-cluster?orgId=1&refresh=10s&from=1685626615866&to=1685630215866&panelId=4'
+          width='450'
+          height='200'
+          frameborder='0'
+        ></iframe>
+        <iframe
+          src='http://localhost:3000/d-solo/efa86fd1d0c121a26444b636a3f509a8/kubernetes-compute-resources-cluster?orgId=1&refresh=10s&from=1685626615866&to=1685630215866&panelId=4'
+          width='450'
+          height='200'
+          frameborder='0'
+        ></iframe>
+      </div>
+      <div className={styles.chartMetrics}>
+        <iframe
+          src='http://localhost:3000/d-solo/efa86fd1d0c121a26444b636a3f509a8/kubernetes-compute-resources-cluster?orgId=1&refresh=10s&from=1685626670249&to=1685630270249&panelId=7'
+          width='450'
+          height='200'
+          frameborder='0'
+        ></iframe>
+        <iframe
+          src='http://localhost:3000/d-solo/efa86fd1d0c121a26444b636a3f509a8/kubernetes-compute-resources-cluster?orgId=1&refresh=10s&from=1685626705202&to=1685630305202&panelId=9'
+          width='450'
+          height='200'
+          frameborder='0'
+        ></iframe>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;

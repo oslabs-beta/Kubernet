8f335b9297aa8d4ca8e9901e5bc9c93cfb817765
Paul <plee50@binghamton.edu>
2023-06-01 18:31:08 -0400

    cahgned width to 800

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -28,13 +28,13 @@ function Dashboard() {
       <div className={styles.chartMetrics}>
         <iframe
           src='http://localhost:3000/d-solo/efa86fd1d0c121a26444b636a3f509a8/kubernetes-compute-resources-cluster?orgId=1&refresh=10s&from=1685626670249&to=1685630270249&panelId=7'
-          width='450'
+          width='800'
           height='200'
           frameborder='0'
         ></iframe>
         <iframe
           src='http://localhost:3000/d-solo/efa86fd1d0c121a26444b636a3f509a8/kubernetes-compute-resources-cluster?orgId=1&refresh=10s&from=1685626705202&to=1685630305202&panelId=9'
-          width='450'
+          width='800'
           height='200'
           frameborder='0'
         ></iframe>

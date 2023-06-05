9286c66699d830777e3b6bde3dcea7c517ca4970
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-05 07:36:07 -0500

    added port forwarding to installController

--- server/controllers/installController.js
+++ server/controllers/installController.js
@@ -19,11 +19,6 @@ installController.promInstall = (req, res, next) => {
     { stdio: 'inherit', shell: true }
   );
 
-  spawnSync(
-    'helm repo add stable https://kubernetes-charts.storage.googleapis.com/',
-    { stdio: 'inherit', shell: true }
-  );
-
   //  Updating the repo
   spawnSync('helm repo update', { stdio: 'inherit', shell: true });
 
@@ -32,7 +27,41 @@ installController.promInstall = (req, res, next) => {
     'helm install prometheusOSP prometheus-community/kube-prometheus-stack',
     { stdio: 'inherit', shell: true }
   );
+
   return next();
+};
+
+/**
+ *  Forwards port to 3000
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @returns
+ */
+installController.portForward = (req, res, next) => {
+  console.log('Forwarding ports to localhost:3000');
+
+  //  Attempts for forward cluster port 3000 to localhost 3000
+  const port = spawn(
+    'kubectl port-forward deployment/prometheus-grafana 3000',
+    { shell: true }
+  );
+
+  port.stdout.on('data', (data) => {
+    console.log('Success! Grafana can now be access on localhost:3000');
+    return next();
+  });
+
+  port.stderr.on('data', (data) => {
+    console.error(`grafana port forwarding error: ${data}`);
+    return next({
+      log: 'Express error handler caught! portForward middleware error',
+      status: 500,
+      message: {
+        err: 'An error occurred while attempting to forward a port to localhost:3000',
+      },
+    });
+  });
 };
 
 module.exports = installController;

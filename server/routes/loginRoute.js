f22674ec7d65ae90ad1a8aed3178dc92a0ce2019
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-06-06 15:58:33 -0400

    Created new route for sign up

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -6,9 +6,6 @@ const cookieController = require('../controllers/cookieController');
 const sessionController = require('../controllers/sessionController');
 
 
-loginRouter.post('/signUp', userController.createUser, sessionController.startSession, cookieController.setSSIDCookie, (req, res) => {
-  return res.sendStatus(200);
-})
 
 loginRouter.post('/loginUser', userController.verifyUser, sessionController.startSession, cookieController.setSSIDCookie, (req,res) => {
   return res.sendStatus(200);

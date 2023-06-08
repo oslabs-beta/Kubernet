52e349572436274907ccc0a08a582f694888ab49
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-08 11:21:06 -0500

    Styled RootLayout

--- client/src/components/RootLayout.jsx
+++ client/src/components/RootLayout.jsx
@@ -6,15 +6,26 @@ function RootLayout() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <nav>
+        <div>
+          {/* Logo here */}
           <NavLink to='/' className={styles.link}>
             Home
           </NavLink>
-        </nav>
+        </div>
+
+        <span className={styles.title}>Kubernét</span>
+
+        <div>
+          <NavLink to='/' className={styles.link}>
+            About
+          </NavLink>
+          {/* Dark/Light mode here */}
+        </div>
       </header>
 
       <main className={styles.outlet}>
         <Outlet />
+        <p className={styles.copy}>© 2023 Kubernét</p>
       </main>
     </div>
   );

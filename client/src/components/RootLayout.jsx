4369b4b70ed8345e5e882405fc1dc5b21e11efc6
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-08 14:48:44 -0500

    added logo

--- client/src/components/RootLayout.jsx
+++ client/src/components/RootLayout.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+
 import styles from '../styles/RootLayout.module.scss';
+
+import logo from '../assets/logo.png';
 
 function RootLayout() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <div>
-          {/* Logo here */}
+        <div className={styles.div}>
+          <img src={logo} alt='pretty logo' className={styles.logo} />
           <NavLink to='/' className={styles.link}>
             Home
           </NavLink>

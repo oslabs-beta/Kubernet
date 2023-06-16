6bf0a980f71a0304f9045dacba6d34e957d84e00
Jimpulse48 <107091480+Jimpulse48@users.noreply.github.com>
2023-06-16 10:14:52 -0500

    converted cookie and grafana controller to TS

--- server/controllers/cookieController.ts
+++ server/controllers/cookieController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 const cookieController = {
   setSSIDCookie: (req: Request, res: Response, next: NextFunction) => {
+    console.log('TypeScript!!!!');
     res.cookie('ssid', res.locals.user, { httpOnly: true });
     return next();
   },

49bf4597894e9b76b80fb3cddbdcb852d40ce451
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-06-17 09:16:11 -0400

    Testing login page

--- __tests__/installation.test.js
+++ __tests__/installation.test.js
@@ -1,18 +1,10 @@
 import React from 'react'
-// import API mocking utilities from Mock Service Worker
-import {rest} from 'msw';
-
-
-// add custom jest matchers from jest-dom
 import '@testing-library/jest-dom'
-
-// import react testing methods
 import { render, screen} from "@testing-library/react"
-
 import '@testing-library/jest-dom/extend-expect';
-import Installation from '../client/src/pages/Installation';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { server } from '../mocks/server';
+import Installation from '../client/src/pages/Installation';
 
 
 beforeAll(() => {
--- __tests__/loginPage.test.js
+++ __tests__/loginPage.test.js
@@ -1,20 +1,55 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import {BrowserRouter as Router} from 'react-router-dom';
 import LoginPage from "../client/src/pages/LoginPage";
 
 describe(LoginPage, () => {
 
   beforeEach(() => {
-    <Router>
-      <LoginPage/>
-    </Router>
-  })
+    render (
+      <Router>
+        <LoginPage/>
+      </Router>
+    )
+    
+  });
 
-  screen.debug()
+  describe('Rendering', () => {
+    test('Checks if Username input field is on the page', () => {
+      expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
+    });
+  
+    test('Checks if Password input field is on the page', () => {
+      expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+    });
+  
+    test('Checks if Login button is on the page', () => {
+      const loginButton = screen.getByRole('button', {name:'Login'});
+      expect(loginButton).toBeInTheDocument();
+    });
+  
+    test("Checks if 'create an account' button is on the page", () => {
+      const createAnAccount = screen.getByRole('button', {name: 'create an account'});
+      expect(createAnAccount).toBeInTheDocument();
+    });
+  });
 
-  test('Checks if username input field is on the page', () => {
-    expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
+  describe('Behavior', () => {
+    test('User types in login details',async () => {
+      const userNameInput = screen.getByPlaceholderText('Username');
+      const passwordInput = screen.getByPlaceholderText('Password');
+   
+      await userEvent.type(userNameInput, 'james');
+      await userEvent.type(passwordInput, 'james123');
+   
+      expect(userNameInput.value).toBe('james');
+      expect(passwordInput.value).toBe('james123');
+    })
+ 
   })
+  
+
+
 })
